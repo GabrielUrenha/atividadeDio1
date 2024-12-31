d96f5d70847a9feabc11fc31a318fce60cb6bc5b
GabrielUrenha <gabr10.gal@gmail.com>
2024-12-30 23:05:24 -0300

    Atividade finalizada

---
+++ ativDio.js
@@ -0,0 +1,24 @@
+let nomeHeroi = "Irineu";
+let xpHeroi = 5000; //insira a quantidade xp
+const mensagemRanking = " está no ranking: ";
+
+for (cont = 1; cont <= xpHeroi; cont++) {
+  console.log("O herói ganhou XP - " + cont)
+  //cont começa em 1 e enquanto cont for menor ou igual a xpHeroi, cont incrimenta +1
+}
+
+if (xpHeroi <= 1000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Ferro");
+} else if (xpHeroi > 1000 && xpHeroi <= 2000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Bronze");
+} else if (xpHeroi > 2000 && xpHeroi <= 5000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Prata/Ouro");
+} else if (xpHeroi > 5000 && xpHeroi <= 8000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Platina/Diamante");
+} else if (xpHeroi > 8000 && xpHeroi <= 9000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Ascendente");
+} else if (xpHeroi > 9000 && xpHeroi <= 10000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Imortal");
+} else if (xpHeroi > 10000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Radiante");
+} 
